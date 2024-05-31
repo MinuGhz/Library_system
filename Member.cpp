f24fec438df1f47e88cf736c00f0ea542017d52d
MinuGhz <minooghandriz@gmail.com>
2024-05-31 23:25:58 +0330

    Member class (completed)

--- Member.cpp
+++ Member.cpp
@@ -3,7 +3,9 @@
 
 using namespace std;
 
-Member::Member() : Person(){}
+Member::Member() : Person(){
+    MemberID = "";
+}
 
 Member::Member(string name, string ID , string MemberID) : Person(name,ID)
 {this->MemberID = MemberID;}
@@ -12,12 +14,32 @@ Member::Member(const Member& someone){
     MemberID = someone.MemberID;
     name = someone.name;
     ID = someone.ID;
+    for(int i=0 ; i<10 ; i++){
+        book[i] = someone.book[i];
+    }
 }
 
 void Member::set_member(string name, string ID, string MemberID) {
     this->name = name;
     this->ID = ID;
     this->MemberID = MemberID;
+}
+
+void Member::set_book(Book &asked) {
+    int i;
+    for(i=0 ; i<10 ; i++){
+        if(book[i].get_book_name() == asked.get_book_name())
+        {cout<<"The book is currently on loan to this member!\n";
+            break;}
+        if(book[i].get_book_name() == ""){
+            book[i] = asked;
+            break;
+        }
+    }
+    if(i==10) {
+        cout<<"this member can't borrow any other book!";
+        cout<<"(because they have currently borrowed 10 book)\n";
+    }
 }
 
 void Member::get_member() const{
@@ -29,14 +51,26 @@ inline string Member::get_memberID() const{
     return MemberID;
 }
 
+void Member::return_book(Book &borrowed) {
+    int i;
+    for(i=0 ; i<10 ; i++){
+        if(book[i].get_book_ID() == borrowed.get_book_ID()) {
+            book[i].set_book("" , "" , false);
+        }
+    }
+    if(i==10) cout<<"The book was not found in Member book list!\n";
+}
 
 ostream& operator << (ostream& output , const Member& someone){
     output<<"StaffID: "<<someone.get_memberID()<<endl;
     output<<"name: "<<someone.get_person_name()<<", ID: ";
     output<<someone.get_person_ID()<<endl;
+    output<<"Borrowed books: \n";
+    for(int i=0 ; i<10 ; i++) output<<someone.book[i];
 
     return output;
 }
+
 
 istream& operator >> (istream& input , Member& someone){
     string name , ID , MemberID;
--- Member.h
+++ Member.h
@@ -9,16 +9,20 @@ class Member : public Person{
 private:
     string MemberID;       /*we considered that everyone has a general ID(ID in Person class)
                            and has a specific ID(staff/member/etc..)*/
-    Book* book;
+    Book book[10];         /*We considered that each member can only borrow
+                           10 books at a time*/
 
 public:
     Member();                                     //default constructor
     Member(string,string,string);
-    Member(const Member&);                         //copy constructor
+    Member(const Member&);                        //copy constructor
 
     void set_member(string,string,string);
+    void set_book(Book&);
     void get_member() const;
     string get_memberID() const;
+
+    void return_book(Book&);
 
 
     friend ostream& operator << (ostream& , const Member&);
